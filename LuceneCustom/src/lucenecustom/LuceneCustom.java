41e29f21cd906d6a551fb28610cfe9cf0a3157a2
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-01-19 14:14:06 +0100

    adding tests to the main

--- LuceneCustom/src/lucenecustom/LuceneCustom.java
+++ LuceneCustom/src/lucenecustom/LuceneCustom.java
@@ -4,6 +4,14 @@
  */
 package lucenecustom;
 
+import java.io.IOException;
+import java.io.StringReader;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.TokenStream;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+import org.apache.lucene.analysis.tokenattributes.OffsetAttribute;
+import org.apache.lucene.util.Version;
+
 /**
  *
  * @author Cyril
@@ -13,7 +21,24 @@ public class LuceneCustom {
   /**
    * @param args the command line arguments
    */
-  public static void main(String[] args) {
-    // TODO code application logic here
+  public static void main(String[] args) throws IOException {
+    Version matchVersion = Version.LUCENE_40; // Substitute desired Lucene version for XY
+    Analyzer analyzer = new MyAnalyser(matchVersion); // or any other analyzer
+    try (TokenStream ts = analyzer.tokenStream("myfield", new StringReader("some text goes here"))) {
+      OffsetAttribute offsetAtt = ts.addAttribute(OffsetAttribute.class);
+      CharTermAttribute termAttr = ts.addAttribute(CharTermAttribute.class);
+
+      ts.reset(); // Resets this stream to the beginning. (Required)
+      
+      while (ts.incrementToken()) {
+        // Use AttributeSource.reflectAsString(boolean)
+        // for token stream debugging.
+        System.out.println("token: " + ts.reflectAsString(true));
+
+        System.out.println("token start offset: " + termAttr.toString());
+      }
+      
+      ts.end();   // Perform end-of-stream operations, e.g. set the final offset.
+    }
   }
 }
